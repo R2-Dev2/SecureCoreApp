013bd7c399d436dfe0f79cb9584547c67d182c74
DSSaumell <dsegurasmll@gmail.com>
2024-11-20 15:50:48 +0100

    feat: Add splash form

--- MainForms/frmSplash.Designer.cs
+++ MainForms/frmSplash.Designer.cs
@@ -30,11 +30,57 @@ namespace MainForms
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.Percentatge = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(157, 345);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(429, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // Percentatge
+            // 
+            this.Percentatge.AutoSize = true;
+            this.Percentatge.BackColor = System.Drawing.Color.Transparent;
+            this.Percentatge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Percentatge.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Percentatge.Location = new System.Drawing.Point(347, 371);
+            this.Percentatge.Name = "Percentatge";
+            this.Percentatge.Size = new System.Drawing.Size(35, 20);
+            this.Percentatge.TabIndex = 1;
+            this.Percentatge.Text = "0%";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmSplash";
+            this.BackgroundImage = global::MainForms.Properties.Resources.SecureCoreSplash;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(731, 414);
+            this.Controls.Add(this.Percentatge);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSplash";
+            this.Text = "Splash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label Percentatge;
+        private System.Windows.Forms.Timer timer;
     }
 }
--- MainForms/frmSplash.cs
+++ MainForms/frmSplash.cs
@@ -16,5 +16,20 @@ namespace MainForms
         {
             InitializeComponent();
         }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar.Increment(1);
+            Percentatge.Text = progressBar.Value.ToString() + "%";
+
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                timer.Stop();
+
+                // Login frmLogin = new Login();
+                // frmLogin.Show();
+
+                this.Hide();
+            }
+        }
     }
 }
