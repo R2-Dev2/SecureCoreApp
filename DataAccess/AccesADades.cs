cceef1a788d53120bb7b14d2c0dbc05f82b3e4c6
Sandy Martos <63091069+SandyMrtss@users.noreply.github.com>
2024-11-27 17:27:29 +0100

    feat: Data Access and Login hashing
    * adds access a dades
    
    * adds data access and login encryption

---
+++ DataAccess/AccesADades.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class AccesADades
+    {
+        private string connectionString;
+        private SqlConnection conn;
+
+        public AccesADades(string connectionString)
+        {
+            ConnectionStringSettings conf = ConfigurationManager.ConnectionStrings[connectionString];
+            if (conf != null) connectionString = conf.ConnectionString;
+            this.connectionString = connectionString;
+            Connectar();
+        }
+
+        private void Connectar()
+        {
+            conn = new SqlConnection(connectionString);
+        }
+
+        public DataSet PortarTaula(string nomTaula)
+        {
+            SqlDataAdapter adapter;
+            DataSet dts = new DataSet();
+
+            string query = $"SELECT * FROM {nomTaula}";
+            adapter = new SqlDataAdapter(query, conn);
+
+            conn.Open();
+
+            adapter.Fill(dts, nomTaula);
+
+            conn.Close();
+
+            return dts;
+        }
+
+        public DataSet PortarPerConsulta(string query)
+        {
+            SqlDataAdapter adapter;
+            DataSet dts = new DataSet();
+
+            adapter = new SqlDataAdapter(query, conn);
+
+            conn.Open();
+
+            adapter.Fill(dts);
+
+            conn.Close();
+
+            return dts;
+        }
+
+        public DataSet PortarPerConsulta(string query, string nomTaula)
+        {
+            SqlDataAdapter adapter;
+            DataSet dts = new DataSet();
+
+            adapter = new SqlDataAdapter(query, conn);
+
+            conn.Open();
+
+            adapter.Fill(dts, nomTaula);
+
+            conn.Close();
+
+            return dts;
+        }
+
+        public int Actualitzar(string query, DataSet dts)
+        {
+            int modificats = -1;
+            conn.Open();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+
+            if (dts.HasChanges())
+            {
+                modificats = adapter.Update(dts.Tables[0]);
+            }
+
+            conn.Close();
+
+            return modificats;
+        }
+
+        public void Executa(string query)
+        {
+            conn.Open();
+            SqlCommand cmd;
+            cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+    }
+}
---
+++ Utils/HashingUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class HashingUtils
+    {
+        private const int SaltByteSize = 24;
+        private const int HashByteSize = 24;
+        private const int HashingIterationsCount = 10101;
+
+        private static string pepper = ConfigurationManager.AppSettings["pepper"];
+
+
+        public static byte[] ComputeHash(string password, byte[] salt,
+                                           int iterations = HashingIterationsCount,
+                                           int hashByteSize = HashByteSize)
+        {
+            Rfc2898DeriveBytes hashGenerator = new Rfc2898DeriveBytes(password, salt);
+            hashGenerator.IterationCount = iterations;
+            return hashGenerator.GetBytes(hashByteSize);
+        }
+
+        public static byte[] GenerateSalt(int saltByteSize = SaltByteSize)
+        {
+            RNGCryptoServiceProvider saltGenerator = new RNGCryptoServiceProvider();
+
+            byte[] salt = new byte[saltByteSize];
+            saltGenerator.GetBytes(salt);
+
+            salt = ComputeHash(pepper, salt);
+
+            return salt;
+        }
+        public static bool VerifyPassword(string password, string salt, string passwordHash)
+        {
+            byte[] saltByte = salt.Split('-').Select(s => byte.Parse(s, System.Globalization.NumberStyles.HexNumber)).ToArray();
+            byte[] computedHash = ComputeHash(password, saltByte);
+            string userPasswordHash = BitConverter.ToString(computedHash);
+            return passwordHash == userPasswordHash;
+        }
+    }
+}
