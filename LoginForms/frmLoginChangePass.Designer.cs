282bb6c3a4e6b6d6be4a57364c67afb306c7b532
STORMC23 <albert.recio@sarria.salesians.cat>
2024-11-29 18:18:45 +0100

    Ad change password and change Login

---
+++ LoginForms/frmLoginChangePass.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace LoginForms
+{
+    partial class frmLoginChangePass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLoginChangePass));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblIncorrect = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.pbBackGround = new System.Windows.Forms.PictureBox();
+            this.txtPasswordValidacio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(531, 28);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(300, 49);
+            this.lblTitle.TabIndex = 17;
+            this.lblTitle.Text = "Secure Core";
+            // 
+            // lblIncorrect
+            // 
+            this.lblIncorrect.AutoSize = true;
+            this.lblIncorrect.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncorrect.ForeColor = System.Drawing.Color.Red;
+            this.lblIncorrect.Location = new System.Drawing.Point(495, 378);
+            this.lblIncorrect.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIncorrect.Name = "lblIncorrect";
+            this.lblIncorrect.Size = new System.Drawing.Size(427, 34);
+            this.lblIncorrect.TabIndex = 16;
+            this.lblIncorrect.Text = "Les contrasenyes no coincideixen.";
+            this.lblIncorrect.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblIncorrect.Visible = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(574, 455);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(257, 58);
+            this.btnLogin.TabIndex = 15;
+            this.btnLogin.Text = "Change password";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPwd.Location = new System.Drawing.Point(534, 186);
+            this.txtPwd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(240, 41);
+            this.txtPwd.TabIndex = 14;
+            this.txtPwd.UseSystemPasswordChar = true;
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPwd.Location = new System.Drawing.Point(528, 129);
+            this.lblPwd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(232, 37);
+            this.lblPwd.TabIndex = 12;
+            this.lblPwd.Text = "New Password";
+            // 
+            // pbBackGround
+            // 
+            this.pbBackGround.BackColor = System.Drawing.Color.Transparent;
+            this.pbBackGround.Image = ((System.Drawing.Image)(resources.GetObject("pbBackGround.Image")));
+            this.pbBackGround.Location = new System.Drawing.Point(13, 14);
+            this.pbBackGround.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbBackGround.Name = "pbBackGround";
+            this.pbBackGround.Size = new System.Drawing.Size(474, 656);
+            this.pbBackGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBackGround.TabIndex = 19;
+            this.pbBackGround.TabStop = false;
+            // 
+            // txtPasswordValidacio
+            // 
+            this.txtPasswordValidacio.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordValidacio.Location = new System.Drawing.Point(535, 302);
+            this.txtPasswordValidacio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPasswordValidacio.Name = "txtPasswordValidacio";
+            this.txtPasswordValidacio.Size = new System.Drawing.Size(240, 41);
+            this.txtPasswordValidacio.TabIndex = 21;
+            this.txtPasswordValidacio.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(529, 245);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 37);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "New Password";
+            // 
+            // frmLoginChangePass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 704);
+            this.Controls.Add(this.txtPasswordValidacio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblIncorrect);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.pbBackGround);
+            this.Name = "frmLoginChangePass";
+            this.Text = "frmLoginChangePasscs";
+            this.Load += new System.EventHandler(this.frmLoginChangePass_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBackGround)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblIncorrect;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.PictureBox pbBackGround;
+        private System.Windows.Forms.TextBox txtPasswordValidacio;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LoginForms/frmLoginChangePass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using DataAccess;
+using MainForms;
+using Utils;
+
+namespace LoginForms
+{
+    public partial class frmLoginChangePass : Form
+    {
+        private AccesADades accesADades;
+        private DataSet dts;
+        private string tableName = "Users";
+        private string password;
+        private string _idUser;
+        private string query;
+
+        public string idUser
+        {
+            get => _idUser;
+            set => _idUser = value;
+        }
+
+        public frmLoginChangePass()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLoginChangePass_Load(object sender, EventArgs e)
+        {
+            this.accesADades = new AccesADades("SecureCore");
+            PortarDades();
+        }
+
+        private void PortarDades()
+        {
+            query= $"SELECT idUser, Password, Salt FROM {this.tableName} WHERE idUser = '{idUser}'";
+
+            dts = accesADades.PortarPerConsulta(query);
+        }
+
+        private void ActualitzarContrasenya(string novaContrasenya)
+        {
+            if(this.dts.Tables[0].Rows.Count == 1)
+            {
+                byte[] salt = HashingUtils.GenerateSalt();
+                byte[] hashedPassword = HashingUtils.ComputeHash(password, salt);
+                this.dts.Tables[0].Rows[0]["Salt"] = BitConverter.ToString(salt);
+                this.dts.Tables[0].Rows[0]["Password"] = BitConverter.ToString(hashedPassword);
+                accesADades.Actualitzar(this.query, dts);
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPwd.Text) || string.IsNullOrWhiteSpace(txtPasswordValidacio.Text))
+            {
+                lblIncorrect.Visible = true;
+            }
+            else if (txtPwd.Text == txtPasswordValidacio.Text)
+            {
+                password = txtPwd.Text;
+
+                ActualitzarContrasenya(password);
+
+                this.Close();
+            }
+            else
+            {
+                lblIncorrect.Visible = true;
+            }
+        }
+    }
+}
