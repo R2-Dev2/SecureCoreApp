cefed30f11980de9e8d4098323dd080a0e644ef5
Eric Sanclimens <ericsancli@gmail.com>
2024-12-10 18:40:53 +0100

    feat: Design & Props

---
+++ CustomControls/SWCodi.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace CustomControls
+{
+    partial class SWCodi
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Enabled = false;
+            this.tbDescription.Location = new System.Drawing.Point(109, 3);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(236, 26);
+            this.tbDescription.TabIndex = 0;
+            // 
+            // tbCode
+            // 
+            this.tbCode.Location = new System.Drawing.Point(3, 3);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(100, 26);
+            this.tbCode.TabIndex = 1;
+            // 
+            // SWCodi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbCode);
+            this.Controls.Add(this.tbDescription);
+            this.Name = "SWCodi";
+            this.Size = new System.Drawing.Size(350, 33);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.TextBox tbCode;
+    }
+}
---
+++ CustomControls/SWCodi.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControls
+{
+    public partial class SWCodi : UserControl
+    {
+        public SWCodi()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private string nomTaula;
+        public string NomTaula
+        {
+            get { return nomTaula; }
+            set { nomTaula = value; }
+        }
+
+        private string nomCodi;
+        public string NomCodi
+        {
+            get { return nomCodi; }
+            set { nomCodi = value; }
+        }
+
+        private string nomDesc;
+        public string NomDesc
+        {
+            get { return nomDesc; }
+            set { nomDesc = value; }
+        }
+
+        private string nomId;
+        public string NomId
+        {
+            get { return nomId; }
+            set { nomId = value; }
+        }
+
+        private string controlId;
+        public string ControlId
+        {
+            get { return controlId; }
+            set { controlId = value; }
+        }
+
+        private string formCS;
+        public string FormCs
+        {
+            get { return formCS; }
+            set { formCS = value; }
+        }
+
+        private string classeCS;
+        public string ClasseCS
+        {
+            get { return classeCS; }
+            set { classeCS = value; }
+        }
+
+        private void Validacodi()
+        {
+            
+        }
+        private void ObreCS()
+        {
+
+        }
+    }
+}
