9a89f8c33320900fcdaf73e699cd6097fafe7a40
STORMC23 <albert.recio@sarria.salesians.cat>
2024-12-12 19:49:49 +0100

    UserContegoryMan

---
+++ UserCategories/frmUserCategories.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace UserCategories
+{
+    partial class frmUserCategories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmUserCategories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 970);
+            this.Name = "frmUserCategories";
+            this.Text = "frmUserCategories";
+            this.Load += new System.EventHandler(this.frmUserCategories_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ UserCategories/frmUserCategories.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MainForms;
+
+namespace UserCategories
+{
+    public partial class frmUserCategories : frmBase
+    {
+        public frmUserCategories()
+        {
+            InitializeComponent();
+            this.Title = "User Categories";
+            tableName = "UserCategories";
+            query = $"SELECT * FROM {tableName}";
+
+        }
+
+        private void frmUserCategories_Load(object sender, EventArgs e)
+        {
+            dtgDades.Columns["idUserCategory"].Visible = false;
+            dtgDades.Columns["CodeCategory"].HeaderText = "Category";
+            dtgDades.Columns["DescCategory"].HeaderText = "Description";
+            dtgDades.Columns["AccessLevel"].HeaderText = "Access Level";
+        }
+    }
+}
