2de536b8ce5c33fde75db0f3891906fd6a4b8c63
STORMC23 <albert.recio@sarria.salesians.cat>
2024-12-12 19:42:47 +0100

    UserMan

--- MainForms/frmUsers.cs
+++ MainForms/frmUsers.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace MainForms
 {
-    public partial class frmUsers : Form
+    public partial class frmUsers : frmBase
     {
         public frmUsers()
         {
             InitializeComponent();
+            this.Title = "Users";
+            tableName = "Users";
+            query = $"SELECT * FROM {tableName}";
+
+        }
+
+        private void frmUsers_Load(object sender, EventArgs e)
+        {
+            dtgDades.Columns["idUser"].Visible = false;
+            dtgDades.Columns["Salt"].Visible = false;
+            dtgDades.Columns["idSpecie"].Visible = false;
+            dtgDades.Columns["idPlanet"].Visible = false;
+            dtgDades.Columns["idUserRank"].Visible = false;
+            dtgDades.Columns["idUserCategory"].Visible = false;
+            dtgDades.Columns["Password"].Visible = false;
+
+            dtgDades.Columns["CodeUser"].HeaderText = "Code";
+            dtgDades.Columns["UserName"].HeaderText = "User";
+            dtgDades.Columns["Photo"].HeaderText = "Image";
         }
     }
 }
