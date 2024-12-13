22465c76c90cd97c1f8c5ac052358358586dbbbd
STORMC23 <albert.recio@sarria.salesians.cat>
2024-12-13 18:16:58 +0100

    David Stussy

--- UserCategories/frmUserCategory.Designer.cs
+++ UserCategories/frmUserCategory.Designer.cs
@@ -29,55 +29,55 @@ namespace UserCategories
         /// </summary>
         private void InitializeComponent()
         {
-            this.swTextBox1 = new CustomControls.SWTextBox();
-            this.swTextBox2 = new CustomControls.SWTextBox();
-            this.swTextBox3 = new CustomControls.SWTextBox();
+            this.tbCode = new CustomControls.SWTextBox();
+            this.tbDesc = new CustomControls.SWTextBox();
+            this.tbAccesLev = new CustomControls.SWTextBox();
             this.lblCode = new System.Windows.Forms.Label();
             this.lblDesc = new System.Windows.Forms.Label();
             this.lblAccLev = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // swTextBox1
+            // tbCode
             // 
-            this.swTextBox1.columnName = null;
-            this.swTextBox1.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
-            this.swTextBox1.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.swTextBox1.isForeignKey = false;
-            this.swTextBox1.Location = new System.Drawing.Point(210, 187);
-            this.swTextBox1.Name = "swTextBox1";
-            this.swTextBox1.opcionalDada = false;
-            this.swTextBox1.required = false;
-            this.swTextBox1.Size = new System.Drawing.Size(268, 41);
-            this.swTextBox1.TabIndex = 22;
-            this.swTextBox1.Tag = "CodeCategory";
+            this.tbCode.columnName = null;
+            this.tbCode.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
+            this.tbCode.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCode.isForeignKey = false;
+            this.tbCode.Location = new System.Drawing.Point(210, 187);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.opcionalDada = false;
+            this.tbCode.required = false;
+            this.tbCode.Size = new System.Drawing.Size(268, 41);
+            this.tbCode.TabIndex = 22;
+            this.tbCode.Tag = "CodeCategory";
             // 
-            // swTextBox2
+            // tbDesc
             // 
-            this.swTextBox2.columnName = null;
-            this.swTextBox2.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
-            this.swTextBox2.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.swTextBox2.isForeignKey = false;
-            this.swTextBox2.Location = new System.Drawing.Point(210, 299);
-            this.swTextBox2.Name = "swTextBox2";
-            this.swTextBox2.opcionalDada = false;
-            this.swTextBox2.required = false;
-            this.swTextBox2.Size = new System.Drawing.Size(268, 41);
-            this.swTextBox2.TabIndex = 23;
-            this.swTextBox2.Tag = "DescCategory";
+            this.tbDesc.columnName = null;
+            this.tbDesc.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
+            this.tbDesc.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDesc.isForeignKey = false;
+            this.tbDesc.Location = new System.Drawing.Point(210, 299);
+            this.tbDesc.Name = "tbDesc";
+            this.tbDesc.opcionalDada = false;
+            this.tbDesc.required = false;
+            this.tbDesc.Size = new System.Drawing.Size(268, 41);
+            this.tbDesc.TabIndex = 23;
+            this.tbDesc.Tag = "DescCategory";
             // 
-            // swTextBox3
+            // tbAccesLev
             // 
-            this.swTextBox3.columnName = null;
-            this.swTextBox3.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
-            this.swTextBox3.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.swTextBox3.isForeignKey = false;
-            this.swTextBox3.Location = new System.Drawing.Point(592, 187);
-            this.swTextBox3.Name = "swTextBox3";
-            this.swTextBox3.opcionalDada = false;
-            this.swTextBox3.required = false;
-            this.swTextBox3.Size = new System.Drawing.Size(268, 41);
-            this.swTextBox3.TabIndex = 24;
-            this.swTextBox3.Tag = "AccessLevel";
+            this.tbAccesLev.columnName = null;
+            this.tbAccesLev.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.tbAccesLev.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAccesLev.isForeignKey = false;
+            this.tbAccesLev.Location = new System.Drawing.Point(592, 187);
+            this.tbAccesLev.Name = "tbAccesLev";
+            this.tbAccesLev.opcionalDada = false;
+            this.tbAccesLev.required = false;
+            this.tbAccesLev.Size = new System.Drawing.Size(268, 41);
+            this.tbAccesLev.TabIndex = 24;
+            this.tbAccesLev.Tag = "AccessLevel";
             // 
             // lblCode
             // 
@@ -114,15 +114,15 @@ namespace UserCategories
             this.Controls.Add(this.lblAccLev);
             this.Controls.Add(this.lblDesc);
             this.Controls.Add(this.lblCode);
-            this.Controls.Add(this.swTextBox3);
-            this.Controls.Add(this.swTextBox2);
-            this.Controls.Add(this.swTextBox1);
+            this.Controls.Add(this.tbAccesLev);
+            this.Controls.Add(this.tbDesc);
+            this.Controls.Add(this.tbCode);
             this.Name = "frmUserCategory";
             this.Text = "frmUserCategory";
             this.Load += new System.EventHandler(this.frmUserCategory_Load);
-            this.Controls.SetChildIndex(this.swTextBox1, 0);
-            this.Controls.SetChildIndex(this.swTextBox2, 0);
-            this.Controls.SetChildIndex(this.swTextBox3, 0);
+            this.Controls.SetChildIndex(this.tbCode, 0);
+            this.Controls.SetChildIndex(this.tbDesc, 0);
+            this.Controls.SetChildIndex(this.tbAccesLev, 0);
             this.Controls.SetChildIndex(this.lblCode, 0);
             this.Controls.SetChildIndex(this.lblDesc, 0);
             this.Controls.SetChildIndex(this.lblAccLev, 0);
@@ -133,9 +133,9 @@ namespace UserCategories
 
         #endregion
 
-        private CustomControls.SWTextBox swTextBox1;
-        private CustomControls.SWTextBox swTextBox2;
-        private CustomControls.SWTextBox swTextBox3;
+        private CustomControls.SWTextBox tbCode;
+        private CustomControls.SWTextBox tbDesc;
+        private CustomControls.SWTextBox tbAccesLev;
         private System.Windows.Forms.Label lblCode;
         private System.Windows.Forms.Label lblDesc;
         private System.Windows.Forms.Label lblAccLev;
