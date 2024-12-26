93267bd76f4905eca3c660337a27737ba0c56a0a
STORMC23 <albert.recio@sarria.salesians.cat>
2024-12-26 18:37:34 +0100

    Search Users

---
+++ UserCategories/frmUserSearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MainForms;
+
+namespace UserCategories
+{
+    public partial class frmUserSearch : frmSearch
+    {
+        public frmUserSearch(string controlId, Form originalForm)
+        {
+            InitializeComponent();
+            this.tableName = "Users";
+            this.title = "Search Users";
+            this.controlId = controlId;
+            this.originalForm = originalForm;
+        }
+        protected override void ConfigurarDataGrid()
+        {
+            base.ConfigurarDataGrid();
+            dtgDades.Columns["idUsers"].Visible = false;
+            dtgDades.Columns["CodeUser"].HeaderText = "Code";
+            dtgDades.Columns["UserName"].HeaderText = "User Name";
+            dtgDades.Columns["Login"].Visible = false;
+            dtgDades.Columns["Password"].Visible = false;
+            dtgDades.Columns["idUserRank"].Visible = false;
+            dtgDades.Columns["idUserCategory"].Visible = false;
+            dtgDades.Columns["Photo"].Visible = false;
+            dtgDades.Columns["idPlanet"].Visible = false;
+            dtgDades.Columns["idSpecie"].Visible = false;
+            dtgDades.Columns["Salt"].Visible = false;
+        }
+
+    }
+}
