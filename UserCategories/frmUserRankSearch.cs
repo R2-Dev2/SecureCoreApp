b08ae7eff2e763216f2beb7b6fb020fd6efa6639
STORMC23 <albert.recio@sarria.salesians.cat>
2024-12-29 19:41:28 +0100

    Search User Ranks

---
+++ UserCategories/frmUserRankSearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MainForms;
+
+namespace UserCategories
+{
+    public partial class frmUserRankSearch : frmSearch
+    {
+        public frmUserRankSearch(string controlId, Form originalForm)
+        {
+            InitializeComponent();
+            this.tableName = "UserRanks";
+            this.title = "Search User Ranks";
+            this.controlId = controlId;
+            this.originalForm = originalForm;
+        }
+        protected override void ConfigurarDataGrid()
+        {
+            base.ConfigurarDataGrid();
+            dtgDades.Columns["idUserRank"].Visible = false;
+            dtgDades.Columns["CodeRank"].HeaderText = "Code";
+            dtgDades.Columns["DescRank"].HeaderText = "Description";
+        }
+    }
+}
