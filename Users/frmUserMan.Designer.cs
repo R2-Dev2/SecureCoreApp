2f54592ce379678ca293a68b9e1597ef52643852
STORMC23 <albert.recio@sarria.salesians.cat>
2025-01-03 18:24:29 +0100

    UserMan sense Crystal part

--- Users/frmUserMan.Designer.cs
+++ Users/frmUserMan.Designer.cs
@@ -33,6 +33,13 @@ namespace Users
             this.lblCode = new System.Windows.Forms.Label();
             this.swtxtUserName = new CustomControls.SWTextBox();
             this.swtxtCode = new CustomControls.SWTextBox();
+            this.swcodRank = new CustomControls.SWCodi();
+            this.swtxtRank = new CustomControls.SWTextBox();
+            this.lblRank = new System.Windows.Forms.Label();
+            this.swUserCat = new CustomControls.SWCodi();
+            this.swTxtUserCategory = new CustomControls.SWTextBox();
+            this.lblUserCategory = new System.Windows.Forms.Label();
+            this.btnViwerUser = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,9 +47,10 @@ namespace Users
             // 
             this.lblUserName.AutoSize = true;
             this.lblUserName.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUserName.Location = new System.Drawing.Point(51, 239);
+            this.lblUserName.Location = new System.Drawing.Point(75, 273);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(94, 20);
+            this.lblUserName.Size = new System.Drawing.Size(155, 33);
             this.lblUserName.TabIndex = 37;
             this.lblUserName.Text = "User Name";
             // 
@@ -50,9 +58,10 @@ namespace Users
             // 
             this.lblCode.AutoSize = true;
             this.lblCode.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCode.Location = new System.Drawing.Point(51, 184);
+            this.lblCode.Location = new System.Drawing.Point(79, 202);
+            this.lblCode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCode.Name = "lblCode";
-            this.lblCode.Size = new System.Drawing.Size(48, 20);
+            this.lblCode.Size = new System.Drawing.Size(79, 33);
             this.lblCode.TabIndex = 36;
             this.lblCode.Text = "Code";
             // 
@@ -63,10 +72,11 @@ namespace Users
             this.swtxtUserName.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
             this.swtxtUserName.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.swtxtUserName.isForeignKey = false;
-            this.swtxtUserName.Location = new System.Drawing.Point(170, 230);
+            this.swtxtUserName.Location = new System.Drawing.Point(258, 273);
+            this.swtxtUserName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swtxtUserName.Name = "swtxtUserName";
             this.swtxtUserName.required = false;
-            this.swtxtUserName.Size = new System.Drawing.Size(296, 29);
+            this.swtxtUserName.Size = new System.Drawing.Size(442, 41);
             this.swtxtUserName.TabIndex = 33;
             // 
             // swtxtCode
@@ -76,21 +86,121 @@ namespace Users
             this.swtxtCode.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
             this.swtxtCode.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.swtxtCode.isForeignKey = false;
-            this.swtxtCode.Location = new System.Drawing.Point(170, 180);
+            this.swtxtCode.Location = new System.Drawing.Point(258, 195);
+            this.swtxtCode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swtxtCode.Name = "swtxtCode";
             this.swtxtCode.required = true;
-            this.swtxtCode.Size = new System.Drawing.Size(186, 29);
+            this.swtxtCode.Size = new System.Drawing.Size(277, 41);
             this.swtxtCode.TabIndex = 32;
+            // 
+            // swcodRank
+            // 
+            this.swcodRank.classeCS = "Users";
+            this.swcodRank.codeName = "CodeRank";
+            this.swcodRank.controlId = "swtxtRank";
+            this.swcodRank.descName = "DescRank";
+            this.swcodRank.formCS = "frmUserRankSearch";
+            this.swcodRank.Location = new System.Drawing.Point(234, 342);
+            this.swcodRank.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.swcodRank.Name = "swcodRank";
+            this.swcodRank.nameId = "idUserRank";
+            this.swcodRank.Size = new System.Drawing.Size(603, 45);
+            this.swcodRank.TabIndex = 38;
+            this.swcodRank.tableName = "UserRanks";
+            // 
+            // swtxtRank
+            // 
+            this.swtxtRank.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.swtxtRank.codiSW = "swcodRank";
+            this.swtxtRank.columnName = "idUserRank";
+            this.swtxtRank.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swtxtRank.Font = new System.Drawing.Font("Cambria", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swtxtRank.isForeignKey = true;
+            this.swtxtRank.Location = new System.Drawing.Point(718, 354);
+            this.swtxtRank.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.swtxtRank.Name = "swtxtRank";
+            this.swtxtRank.required = true;
+            this.swtxtRank.Size = new System.Drawing.Size(105, 25);
+            this.swtxtRank.TabIndex = 40;
+            // 
+            // lblRank
+            // 
+            this.lblRank.AutoSize = true;
+            this.lblRank.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRank.Location = new System.Drawing.Point(75, 354);
+            this.lblRank.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRank.Name = "lblRank";
+            this.lblRank.Size = new System.Drawing.Size(149, 33);
+            this.lblRank.TabIndex = 39;
+            this.lblRank.Text = "User Rank";
+            // 
+            // swUserCat
+            // 
+            this.swUserCat.classeCS = "Users";
+            this.swUserCat.codeName = "CodeCategory";
+            this.swUserCat.controlId = "swTxtUserCategory";
+            this.swUserCat.descName = "DescCategory";
+            this.swUserCat.formCS = "frmUserCategoriesSearch";
+            this.swUserCat.Location = new System.Drawing.Point(289, 464);
+            this.swUserCat.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.swUserCat.Name = "swUserCat";
+            this.swUserCat.nameId = "idUserCategory";
+            this.swUserCat.Size = new System.Drawing.Size(603, 45);
+            this.swUserCat.TabIndex = 41;
+            this.swUserCat.tableName = "UserCategories";
+            // 
+            // swTxtUserCategory
+            // 
+            this.swTxtUserCategory.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.swTxtUserCategory.codiSW = "swUserCat";
+            this.swTxtUserCategory.columnName = "idUserCategory";
+            this.swTxtUserCategory.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swTxtUserCategory.Font = new System.Drawing.Font("Cambria", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swTxtUserCategory.isForeignKey = true;
+            this.swTxtUserCategory.Location = new System.Drawing.Point(762, 415);
+            this.swTxtUserCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.swTxtUserCategory.Name = "swTxtUserCategory";
+            this.swTxtUserCategory.required = true;
+            this.swTxtUserCategory.Size = new System.Drawing.Size(105, 25);
+            this.swTxtUserCategory.TabIndex = 43;
+            // 
+            // lblUserCategory
+            // 
+            this.lblUserCategory.AutoSize = true;
+            this.lblUserCategory.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserCategory.Location = new System.Drawing.Point(75, 415);
+            this.lblUserCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserCategory.Name = "lblUserCategory";
+            this.lblUserCategory.Size = new System.Drawing.Size(196, 33);
+            this.lblUserCategory.TabIndex = 42;
+            this.lblUserCategory.Text = "User Category";
+            // 
+            // btnViwerUser
+            // 
+            this.btnViwerUser.Location = new System.Drawing.Point(646, 1006);
+            this.btnViwerUser.Name = "btnViwerUser";
+            this.btnViwerUser.Size = new System.Drawing.Size(129, 60);
+            this.btnViwerUser.TabIndex = 44;
+            this.btnViwerUser.Text = "View";
+            this.btnViwerUser.UseVisualStyleBackColor = true;
             // 
             // frmUserMan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 706);
+            this.ClientSize = new System.Drawing.Size(1179, 1103);
+            this.Controls.Add(this.btnViwerUser);
+            this.Controls.Add(this.swUserCat);
+            this.Controls.Add(this.swTxtUserCategory);
+            this.Controls.Add(this.lblUserCategory);
+            this.Controls.Add(this.swcodRank);
+            this.Controls.Add(this.swtxtRank);
+            this.Controls.Add(this.lblRank);
             this.Controls.Add(this.lblUserName);
             this.Controls.Add(this.lblCode);
             this.Controls.Add(this.swtxtUserName);
             this.Controls.Add(this.swtxtCode);
+            this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "frmUserMan";
             this.Text = "frmUserMan";
             this.Controls.SetChildIndex(this.pbClose, 0);
@@ -98,6 +208,13 @@ namespace Users
             this.Controls.SetChildIndex(this.swtxtUserName, 0);
             this.Controls.SetChildIndex(this.lblCode, 0);
             this.Controls.SetChildIndex(this.lblUserName, 0);
+            this.Controls.SetChildIndex(this.lblRank, 0);
+            this.Controls.SetChildIndex(this.swtxtRank, 0);
+            this.Controls.SetChildIndex(this.swcodRank, 0);
+            this.Controls.SetChildIndex(this.lblUserCategory, 0);
+            this.Controls.SetChildIndex(this.swTxtUserCategory, 0);
+            this.Controls.SetChildIndex(this.swUserCat, 0);
+            this.Controls.SetChildIndex(this.btnViwerUser, 0);
             ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -109,5 +226,12 @@ namespace Users
         private System.Windows.Forms.Label lblCode;
         private CustomControls.SWTextBox swtxtUserName;
         private CustomControls.SWTextBox swtxtCode;
+        private CustomControls.SWCodi swcodRank;
+        private CustomControls.SWTextBox swtxtRank;
+        private System.Windows.Forms.Label lblRank;
+        private CustomControls.SWCodi swUserCat;
+        private CustomControls.SWTextBox swTxtUserCategory;
+        private System.Windows.Forms.Label lblUserCategory;
+        private System.Windows.Forms.Button btnViwerUser;
     }
 }
