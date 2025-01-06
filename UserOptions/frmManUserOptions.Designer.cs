f397e9e68b8e160b2ab94e2af99dada6523203fe
DSSaumell <dsegurasmll@gmail.com>
2025-01-06 16:33:55 +0100

    feat: Add UserOptions form with an error.

---
+++ UserOptions/frmManUserOptions.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace UserOptions
+{
+    partial class frmManUserOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.swtxtDescription = new CustomControls.SWTextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.swtxtFormName = new CustomControls.SWTextBox();
+            this.lblFormName = new System.Windows.Forms.Label();
+            this.swtxtTableName = new CustomControls.SWTextBox();
+            this.lblTableName = new System.Windows.Forms.Label();
+            this.lblRangeOption = new System.Windows.Forms.Label();
+            this.lblNamespace = new System.Windows.Forms.Label();
+            this.swtxtRangeOption = new CustomControls.SWTextBox();
+            this.swtxtNamespace = new CustomControls.SWTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbClose
+            // 
+            this.pbClose.Location = new System.Drawing.Point(775, 0);
+            // 
+            // swtxtDescription
+            // 
+            this.swtxtDescription.codiSW = null;
+            this.swtxtDescription.columnName = null;
+            this.swtxtDescription.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swtxtDescription.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swtxtDescription.isForeignKey = false;
+            this.swtxtDescription.Location = new System.Drawing.Point(178, 97);
+            this.swtxtDescription.Name = "swtxtDescription";
+            this.swtxtDescription.required = false;
+            this.swtxtDescription.Size = new System.Drawing.Size(225, 29);
+            this.swtxtDescription.TabIndex = 0;
+            this.swtxtDescription.Tag = "description";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold);
+            this.lblDescription.Location = new System.Drawing.Point(64, 106);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(100, 20);
+            this.lblDescription.TabIndex = 24;
+            this.lblDescription.Text = "Description";
+            // 
+            // swtxtFormName
+            // 
+            this.swtxtFormName.codiSW = null;
+            this.swtxtFormName.columnName = null;
+            this.swtxtFormName.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swtxtFormName.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swtxtFormName.isForeignKey = false;
+            this.swtxtFormName.Location = new System.Drawing.Point(178, 142);
+            this.swtxtFormName.Name = "swtxtFormName";
+            this.swtxtFormName.required = false;
+            this.swtxtFormName.Size = new System.Drawing.Size(225, 29);
+            this.swtxtFormName.TabIndex = 1;
+            this.swtxtFormName.Tag = "formName";
+            // 
+            // lblFormName
+            // 
+            this.lblFormName.AutoSize = true;
+            this.lblFormName.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold);
+            this.lblFormName.Location = new System.Drawing.Point(70, 151);
+            this.lblFormName.Name = "lblFormName";
+            this.lblFormName.Size = new System.Drawing.Size(95, 20);
+            this.lblFormName.TabIndex = 26;
+            this.lblFormName.Text = "FormName";
+            // 
+            // swtxtTableName
+            // 
+            this.swtxtTableName.codiSW = null;
+            this.swtxtTableName.columnName = null;
+            this.swtxtTableName.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swtxtTableName.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swtxtTableName.isForeignKey = false;
+            this.swtxtTableName.Location = new System.Drawing.Point(178, 188);
+            this.swtxtTableName.Name = "swtxtTableName";
+            this.swtxtTableName.required = false;
+            this.swtxtTableName.Size = new System.Drawing.Size(225, 29);
+            this.swtxtTableName.TabIndex = 2;
+            // 
+            // lblTableName
+            // 
+            this.lblTableName.AutoSize = true;
+            this.lblTableName.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold);
+            this.lblTableName.Location = new System.Drawing.Point(64, 197);
+            this.lblTableName.Name = "lblTableName";
+            this.lblTableName.Size = new System.Drawing.Size(101, 20);
+            this.lblTableName.TabIndex = 28;
+            this.lblTableName.Text = "Table Name";
+            // 
+            // lblRangeOption
+            // 
+            this.lblRangeOption.AutoSize = true;
+            this.lblRangeOption.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold);
+            this.lblRangeOption.Location = new System.Drawing.Point(51, 244);
+            this.lblRangeOption.Name = "lblRangeOption";
+            this.lblRangeOption.Size = new System.Drawing.Size(114, 20);
+            this.lblRangeOption.TabIndex = 29;
+            this.lblRangeOption.Text = "Range Option";
+            // 
+            // lblNamespace
+            // 
+            this.lblNamespace.AutoSize = true;
+            this.lblNamespace.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold);
+            this.lblNamespace.Location = new System.Drawing.Point(67, 290);
+            this.lblNamespace.Name = "lblNamespace";
+            this.lblNamespace.Size = new System.Drawing.Size(98, 20);
+            this.lblNamespace.TabIndex = 30;
+            this.lblNamespace.Text = "Namespace";
+            // 
+            // swtxtRangeOption
+            // 
+            this.swtxtRangeOption.codiSW = null;
+            this.swtxtRangeOption.columnName = null;
+            this.swtxtRangeOption.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swtxtRangeOption.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swtxtRangeOption.isForeignKey = false;
+            this.swtxtRangeOption.Location = new System.Drawing.Point(178, 235);
+            this.swtxtRangeOption.Name = "swtxtRangeOption";
+            this.swtxtRangeOption.required = false;
+            this.swtxtRangeOption.Size = new System.Drawing.Size(225, 29);
+            this.swtxtRangeOption.TabIndex = 3;
+            // 
+            // swtxtNamespace
+            // 
+            this.swtxtNamespace.codiSW = null;
+            this.swtxtNamespace.columnName = null;
+            this.swtxtNamespace.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.swtxtNamespace.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.swtxtNamespace.isForeignKey = false;
+            this.swtxtNamespace.Location = new System.Drawing.Point(178, 281);
+            this.swtxtNamespace.Name = "swtxtNamespace";
+            this.swtxtNamespace.required = false;
+            this.swtxtNamespace.Size = new System.Drawing.Size(225, 29);
+            this.swtxtNamespace.TabIndex = 4;
+            // 
+            // frmManUserOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 720);
+            this.Controls.Add(this.swtxtNamespace);
+            this.Controls.Add(this.swtxtRangeOption);
+            this.Controls.Add(this.lblNamespace);
+            this.Controls.Add(this.lblRangeOption);
+            this.Controls.Add(this.lblTableName);
+            this.Controls.Add(this.swtxtTableName);
+            this.Controls.Add(this.lblFormName);
+            this.Controls.Add(this.swtxtFormName);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.swtxtDescription);
+            this.Name = "frmManUserOptions";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.swtxtDescription, 0);
+            this.Controls.SetChildIndex(this.lblDescription, 0);
+            this.Controls.SetChildIndex(this.swtxtFormName, 0);
+            this.Controls.SetChildIndex(this.lblFormName, 0);
+            this.Controls.SetChildIndex(this.swtxtTableName, 0);
+            this.Controls.SetChildIndex(this.lblTableName, 0);
+            this.Controls.SetChildIndex(this.lblRangeOption, 0);
+            this.Controls.SetChildIndex(this.lblNamespace, 0);
+            this.Controls.SetChildIndex(this.swtxtRangeOption, 0);
+            this.Controls.SetChildIndex(this.swtxtNamespace, 0);
+            this.Controls.SetChildIndex(this.pbClose, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomControls.SWTextBox swtxtDescription;
+        private System.Windows.Forms.Label lblDescription;
+        private CustomControls.SWTextBox swtxtFormName;
+        private System.Windows.Forms.Label lblFormName;
+        private CustomControls.SWTextBox swtxtTableName;
+        private System.Windows.Forms.Label lblTableName;
+        private System.Windows.Forms.Label lblRangeOption;
+        private System.Windows.Forms.Label lblNamespace;
+        private CustomControls.SWTextBox swtxtRangeOption;
+        private CustomControls.SWTextBox swtxtNamespace;
+    }
+}
---
+++ UserOptions/frmManUserOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MainForms;
+
+namespace UserOptions
+{
+    public partial class frmManUserOptions : frmBase
+    {
+        public frmManUserOptions()
+        {
+            this.title = "User Options Manteniment";
+            this.tableName = "UserOptions";
+        }
+        protected override void ConfigurarDataGrid()
+        {
+            base.ConfigurarDataGrid();
+            dtgDades.Columns["idOption"].Visible = false;
+            dtgDades.Columns["description"].HeaderText = "Description";
+            dtgDades.Columns["image"].Visible = false;
+            dtgDades.Columns["formName"].HeaderText = "FormName";
+            dtgDades.Columns["tableName"].HeaderText = "TableName";
+            dtgDades.Columns["rangeOption"].HeaderText = "RangeOption";
+            dtgDades.Columns["namespace"].HeaderText = "Namespace";
+        }
+    }
+}
