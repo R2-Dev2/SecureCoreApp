db4d93f4876f92beeb899b6ead1df96f14b41399
Sandy Martos <bebesubi2@gmail.com>
2025-01-07 11:27:32 +0100

    design updates and missing changes

--- Users/frmUserMan.cs
+++ Users/frmUserMan.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
 using MainForms;
 
 namespace Users
 {
     public partial class frmUserMan : frmBase
     {
+        private ReportDocument cryRpt;
         public frmUserMan()
         {
             InitializeComponent();
@@ -34,6 +38,39 @@ namespace Users
             dtgDades.Columns["idPlanet"].Visible = false;
             dtgDades.Columns["idSpecie"].Visible = false;
             dtgDades.Columns["Salt"].Visible = false;
+        }
+
+        private void btnViwerUser_Click(object sender, EventArgs e)
+        {
+            string printer = "Microsoft to PDF";
+            foreach(string str in PrinterSettings.InstalledPrinters)
+            {
+                if(str.ToUpper().Contains(" PDF"))
+                {
+                    printer = str;
+                    break;
+                }
+            }
+            try
+            {
+                cryRpt.PrintOptions.PrinterName = printer;
+                PrinterSettings settings = new PrinterSettings();
+                settings.PrintFileName = "Access_Cards";
+                cryRpt.PrintToPrinter(1, false, 0, 0);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Couldn't find PDF printer");
+            }
+        }
+
+        private void frmUserMan_Load(object sender, EventArgs e)
+        {
+            cryRpt = new ReportDocument();
+            cryRpt.Load("AccessCardsUsers.rpt");
+            crvAccessCards.ReportSource = cryRpt;
+            crvAccessCards.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None; 
+            crvAccessCards.Refresh();
         }
     }
 }
