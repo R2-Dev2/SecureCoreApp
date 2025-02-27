09e83e8ea14079fb1e3ef2d777cd93a416522c2d
STORMC23 <albert.recio@sarria.salesians.cat>
2025-02-27 17:09:27 +0100

    adds factory CRUD with EF
    * Man Factories
    
    * Man Factories complet
    
    * Man Factories complet

---
+++ Factories/frmFactoriesMan.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Factories
+{
+    partial class frmFactoriesMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFactoriesMan));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblSave = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.pbClose = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dtgFactories = new System.Windows.Forms.DataGridView();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.txtDesc = new CustomControls.SWTextBox();
+            this.txtCode = new CustomControls.SWTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgFactories)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSave
+            // 
+            this.lblSave.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSave.Location = new System.Drawing.Point(317, 885);
+            this.lblSave.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSave.Name = "lblSave";
+            this.lblSave.Size = new System.Drawing.Size(450, 31);
+            this.lblSave.TabIndex = 104;
+            this.lblSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblSave.Visible = false;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(54, 382);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(646, 33);
+            this.lblError.TabIndex = 103;
+            this.lblError.Text = "One or more mandatory fields are blank or incorrect";
+            this.lblError.Visible = false;
+            // 
+            // pbClose
+            // 
+            this.pbClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbClose.BackColor = System.Drawing.Color.Transparent;
+            this.pbClose.Image = ((System.Drawing.Image)(resources.GetObject("pbClose.Image")));
+            this.pbClose.Location = new System.Drawing.Point(1104, 9);
+            this.pbClose.Margin = new System.Windows.Forms.Padding(0);
+            this.pbClose.Name = "pbClose";
+            this.pbClose.Size = new System.Drawing.Size(38, 36);
+            this.pbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbClose.TabIndex = 102;
+            this.pbClose.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(110, 79);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(0, 62);
+            this.lblTitle.TabIndex = 101;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(776, 871);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(146, 61);
+            this.btnNew.TabIndex = 106;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.CausesValidation = false;
+            this.btnUpdate.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(930, 871);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(146, 61);
+            this.btnUpdate.TabIndex = 105;
+            this.btnUpdate.Text = "Save";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dtgFactories
+            // 
+            this.dtgFactories.AllowUserToAddRows = false;
+            this.dtgFactories.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgFactories.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dtgFactories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgFactories.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dtgFactories.Location = new System.Drawing.Point(60, 418);
+            this.dtgFactories.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtgFactories.MultiSelect = false;
+            this.dtgFactories.Name = "dtgFactories";
+            this.dtgFactories.ReadOnly = true;
+            this.dtgFactories.RowHeadersVisible = false;
+            this.dtgFactories.RowHeadersWidth = 51;
+            this.dtgFactories.RowTemplate.Height = 24;
+            this.dtgFactories.Size = new System.Drawing.Size(1014, 420);
+            this.dtgFactories.StandardTab = true;
+            this.dtgFactories.TabIndex = 107;
+            this.dtgFactories.SelectionChanged += new System.EventHandler(this.dtgFactories_SelectionChanged);
+            this.dtgFactories.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dtgFactories_KeyDown);
+            // 
+            // lblCode
+            // 
+            this.lblCode.AutoSize = true;
+            this.lblCode.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCode.Location = new System.Drawing.Point(54, 201);
+            this.lblCode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(79, 33);
+            this.lblCode.TabIndex = 108;
+            this.lblCode.Text = "Code";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc.Location = new System.Drawing.Point(54, 280);
+            this.lblDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(167, 33);
+            this.lblDesc.TabIndex = 109;
+            this.lblDesc.Text = "Description";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.codiSW = null;
+            this.txtDesc.columnName = null;
+            this.txtDesc.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.txtDesc.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDesc.isForeignKey = false;
+            this.txtDesc.Location = new System.Drawing.Point(228, 272);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.required = false;
+            this.txtDesc.Size = new System.Drawing.Size(246, 31);
+            this.txtDesc.TabIndex = 113;
+            // 
+            // txtCode
+            // 
+            this.txtCode.codiSW = null;
+            this.txtCode.columnName = null;
+            this.txtCode.dadaPermesa = CustomControls.SWTextBox.TipusDada.Nombre;
+            this.txtCode.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.isForeignKey = false;
+            this.txtCode.Location = new System.Drawing.Point(140, 193);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.required = false;
+            this.txtCode.Size = new System.Drawing.Size(238, 31);
+            this.txtCode.TabIndex = 112;
+            // 
+            // frmFactoriesMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 1038);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblCode);
+            this.Controls.Add(this.lblSave);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.pbClose);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dtgFactories);
+            this.Name = "frmFactoriesMan";
+            this.Text = "frmFactoriesMan";
+            this.Load += new System.EventHandler(this.frmFactoriesMan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgFactories)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSave;
+        private System.Windows.Forms.Label lblError;
+        protected System.Windows.Forms.PictureBox pbClose;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnUpdate;
+        protected System.Windows.Forms.DataGridView dtgFactories;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.TextBox textBox2;
+        private CustomControls.SWTextBox txtCode;
+        private CustomControls.SWTextBox txtDesc;
+    }
+}
---
+++ Factories/frmFactoriesMan.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.IO;
+
+namespace Factories
+{
+    public partial class frmFactoriesMan : Form
+    {
+        SecureCoreG7Entities db;
+        List<Factory> factory;
+        protected string codeTable;
+        bool EsNou = false;
+        private Factory factorySeleccionada;
+        private Factory factoryAnterior;
+
+        public frmFactoriesMan()
+        {
+            InitializeComponent();
+        }
+
+        private void RemoveBinding()
+        {
+            txtCode.DataBindings.Clear();
+            txtCode.Clear();
+            txtDesc.DataBindings.Clear();
+            txtDesc.Clear();
+        }
+
+        private void LoadData()
+        {
+            db = new SecureCoreG7Entities();
+
+            factory = db.Factories.ToList();
+
+            dtgFactories.DataSource = factory;
+            dtgFactories.Refresh();
+            dtgFactories.Update();
+            dtgFactories.Columns["idFactory"].Visible = false;
+            dtgFactories.Columns["codeFactory"].Visible = true;
+            dtgFactories.Columns["DescFactory"].Visible = true;
+
+            dtgFactories.Refresh();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            if (factorySeleccionada != null)
+            {
+                factoryAnterior = new Factory
+                {
+                    codeFactory = factorySeleccionada.codeFactory,
+                    DescFactory = factorySeleccionada.DescFactory
+                };
+            }
+
+            EsNou = true;
+            RemoveBinding();
+            txtCode.Focus();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCode.Text) && string.IsNullOrEmpty(txtDesc.Text))
+            {
+                MessageBox.Show("Por favor, complete todos los campos antes de guardar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (EsNou)
+            {
+                factorySeleccionada = new Factory
+                {
+                    codeFactory = txtCode.Text,
+                    DescFactory = txtDesc.Text,
+                };
+                db.Factories.Add(factorySeleccionada); 
+            }
+            else
+            {
+                if (factorySeleccionada != null)
+                {
+                    factorySeleccionada.codeFactory = txtCode.Text;
+                    factorySeleccionada.DescFactory = txtDesc.Text;
+                    db.Entry(factorySeleccionada).State = EntityState.Modified; 
+                }
+            }
+
+            db.SaveChanges();
+            LoadData(); 
+            EsNou = false;
+        }
+
+        private void dtgFactories_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && dtgFactories.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dtgFactories.SelectedRows)
+                {
+                    int valor = Convert.ToInt32(row.Cells["idFactory"].Value);
+                    var proDel = db.Factories.FirstOrDefault(x => x.idFactory == valor);
+
+                    if (proDel != null)
+                    {
+                        db.Factories.Remove(proDel);  
+                    }
+                }
+
+                db.SaveChanges();
+                LoadData();
+            }
+        }
+
+        private void frmFactoriesMan_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            dtgFactories.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void dtgFactories_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dtgFactories.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dtgFactories.SelectedRows[0];
+
+                int idFactory = Convert.ToInt32(selectedRow.Cells["idFactory"].Value);
+
+                factorySeleccionada = db.Factories.FirstOrDefault(f => f.idFactory == idFactory);
+
+                if (factorySeleccionada != null)
+                {
+                    txtCode.Text = factorySeleccionada.codeFactory;
+                    txtDesc.Text = factorySeleccionada.DescFactory;
+                }
+            }
+        }
+    }
+}
