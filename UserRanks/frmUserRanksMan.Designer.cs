82b82b101912caefb54ce694075b5fe837442c1f
STORMC23 <albert.recio@sarria.salesians.cat>
2025-02-28 16:31:22 +0100

    FACTORIES (#56)

--- UserRanks/frmUserRanksMan.Designer.cs
+++ UserRanks/frmUserRanksMan.Designer.cs
@@ -36,13 +36,17 @@ namespace UserRanks
             ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
             this.SuspendLayout();
             // 
+            // pbClose
+            // 
+            // 
             // lblCodeUserRank
             // 
             this.lblCodeUserRank.AutoSize = true;
             this.lblCodeUserRank.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCodeUserRank.Location = new System.Drawing.Point(103, 219);
+            this.lblCodeUserRank.Location = new System.Drawing.Point(154, 342);
+            this.lblCodeUserRank.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCodeUserRank.Name = "lblCodeUserRank";
-            this.lblCodeUserRank.Size = new System.Drawing.Size(48, 20);
+            this.lblCodeUserRank.Size = new System.Drawing.Size(79, 33);
             this.lblCodeUserRank.TabIndex = 32;
             this.lblCodeUserRank.Text = "Code";
             // 
@@ -50,9 +54,10 @@ namespace UserRanks
             // 
             this.lblDescUserRank.AutoSize = true;
             this.lblDescUserRank.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescUserRank.Location = new System.Drawing.Point(51, 272);
+            this.lblDescUserRank.Location = new System.Drawing.Point(76, 425);
+            this.lblDescUserRank.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDescUserRank.Name = "lblDescUserRank";
-            this.lblDescUserRank.Size = new System.Drawing.Size(100, 20);
+            this.lblDescUserRank.Size = new System.Drawing.Size(167, 33);
             this.lblDescUserRank.TabIndex = 31;
             this.lblDescUserRank.Text = "Description";
             // 
@@ -63,10 +68,11 @@ namespace UserRanks
             this.swDescUserRank.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
             this.swDescUserRank.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.swDescUserRank.isForeignKey = false;
-            this.swDescUserRank.Location = new System.Drawing.Point(166, 268);
+            this.swDescUserRank.Location = new System.Drawing.Point(249, 419);
+            this.swDescUserRank.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swDescUserRank.Name = "swDescUserRank";
             this.swDescUserRank.required = false;
-            this.swDescUserRank.Size = new System.Drawing.Size(272, 29);
+            this.swDescUserRank.Size = new System.Drawing.Size(406, 41);
             this.swDescUserRank.TabIndex = 30;
             this.swDescUserRank.Tag = "DescRank";
             // 
@@ -77,23 +83,25 @@ namespace UserRanks
             this.swCodeUserRank.dadaPermesa = CustomControls.SWTextBox.TipusDada.Text;
             this.swCodeUserRank.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.swCodeUserRank.isForeignKey = false;
-            this.swCodeUserRank.Location = new System.Drawing.Point(166, 215);
+            this.swCodeUserRank.Location = new System.Drawing.Point(249, 336);
+            this.swCodeUserRank.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swCodeUserRank.Name = "swCodeUserRank";
             this.swCodeUserRank.required = true;
-            this.swCodeUserRank.Size = new System.Drawing.Size(187, 29);
+            this.swCodeUserRank.Size = new System.Drawing.Size(278, 41);
             this.swCodeUserRank.TabIndex = 29;
             this.swCodeUserRank.Tag = "CodeRank";
             // 
-            // frmManUserRanks
+            // frmUserRanksMan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 706);
+            this.ClientSize = new System.Drawing.Size(1179, 1103);
             this.Controls.Add(this.lblCodeUserRank);
             this.Controls.Add(this.lblDescUserRank);
             this.Controls.Add(this.swDescUserRank);
             this.Controls.Add(this.swCodeUserRank);
-            this.Name = "frmManUserRanks";
+            this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.Name = "frmUserRanksMan";
             this.Text = "frmManUserRanks";
             this.Controls.SetChildIndex(this.pbClose, 0);
             this.Controls.SetChildIndex(this.swCodeUserRank, 0);
